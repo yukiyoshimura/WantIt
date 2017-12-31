2c8c01cbb17c555c4f4e4cccf0a432b695c32bc7
yukiyoshimura <yuukiyy0234@gmail.com>
2017-12-31 14:47:35 +0900

    #5 modelに定義を出力するgemを追加

--- app/models/want_list.rb
+++ app/models/want_list.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: want_lists
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  product    :string(255)
+#  url        :string(255)
+#  comment    :string(255)
+#  score      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class WantList < ApplicationRecord
 end
