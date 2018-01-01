e3e94cf1095fa1731bd2e07a75f23888ef108b5e
yukiyoshimura <yuukiyy0234@gmail.com>
2018-01-01 17:01:06 +0900

    #1 忘年会景品を登録できる

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.4'
+gem 'rails', '~> 5.1.4'
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.3.18', '< 0.5'
 # Use Puma as the app server
@@ -38,7 +38,7 @@ gem 'jbuilder', '~> 2.5'
 # bootstrap
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 
-# migrate
+# annotate model routes
 gem 'annotate'
 
 group :development, :test do
--- app/controllers/show_list_controller.rb
+++ app/controllers/show_list_controller.rb
@@ -1,5 +1,54 @@
 class ShowListController < ApplicationController
+
+  def new
+    @wantlists = WantList.new
+    p @wantlists
+
+  end
+
   def index
+    p "start index"
     @wantlists = WantList.all
   end
+
+  def create
+    p "start create"
+    p wantlist_params
+
+    input_data = wantlist_params
+
+    @wantlists = WantList.new(
+        {name:input_data[:name],
+         product:input_data[:product],
+         url:input_data[:url],
+         comment:input_data[:comment]
+         }
+    )
+
+    p "after"
+    p @wantlists
+
+    respond_to do |format|
+      if @wantlists.save
+        format.html { redirect_to @wantlists, notice: 'User was successfully created.' }
+        format.json { render :index, status: :created, location: @wantlists }
+        format.js { @status = "success"}
+      else
+        format.html { render :new }
+        format.json { render json: @wantlists.errors, status: :unprocessable_entity }
+        format.js { @status = "fail" }
+      end
+    end
+
+    redirect_to :action => "index"
+  end
+  
+  private
+  
+  def wantlist_params
+    p 'start wantlist_prams'
+    params.require(:want_list)
+  end
+  
+  
 end
