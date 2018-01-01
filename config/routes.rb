6ffc5cc92ace5149f366518348aff39d04bbb792
yukiyoshimura <yuukiyy0234@gmail.com>
2018-01-01 17:05:26 +0900

    Gemfile.lockを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
+# == Route Map
+#
+#           Prefix Verb URI Pattern                 Controller#Action
+#             root GET  /                           show_list#index
+#  show_list_index GET  /show_list/index(.:format)  show_list#index
+#    show_list_new GET  /show_list/new(.:format)    show_list#new
+# show_list_create POST /show_list/create(.:format) show_list#create
+# 
+
 Rails.application.routes.draw do
   root 'show_list#index'
   get 'show_list/index'
+  get 'show_list/new'
+  post 'show_list/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
