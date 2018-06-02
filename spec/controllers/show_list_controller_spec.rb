0ff612523c3c33475fa3b6de4128ee6fafd848c5
yukiyoshimura <yuukiyy0234@gmail.com>
2018-06-03 01:10:22 +0900

    show_list_controllerのコントローラspec追加

--- spec/controllers/show_list_controller_spec.rb
+++ spec/controllers/show_list_controller_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 describe ShowListController do
   describe 'GET #index' do
-
-    let!(:test) { build(:user) }
-    it 'show all list' do
+    before do
       get :index
-      expect(response).to render_template :index
-      p response
+    end
+    context '1件' do
+      let(:wantlists) { create(:wantlist) }
+      it '欲しいものリストを取得する' do
+          expect(response.status).to eq 200
+          expect(assigns(:wantlists)).to eq [wantlists]
+          expect(response).to render_template :index
+      end
+    end
+    context '2件' do
+      let(:wantlists) { create_list(:wantlist, 2)}
+      it '欲しいものリストを取得する' do
+        expect(response.status).to eq 200
+        expect(assigns(:wantlists)).to eq wantlists
+        expect(response).to render_template :index
+      end
     end
   end
 end
