53e655b84db80c929ef50336fdf149050701d211
yukiyoshimura <yuukiyy0234@gmail.com>
2018-07-29 19:42:37 +0900

    モーダルフォームの必須項目がわかるようにメッセージ追加
    テスト周りのソースをコミット

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,10 @@ require 'securerandom'
 
 FactoryBot.define do
   factory :user do
-    email "bbbcd@com"
+
+    sequence :email do |n|
+      "abcd#{n}@com"
+    end
     password SecureRandom.urlsafe_base64
     created_at Time.now
     updated_at Time.now
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,8 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # トランザクションの使用はしないので無効にする
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
